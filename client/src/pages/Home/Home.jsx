2d7b0677b08fc502239b4fa31b40abd2ee7d1678
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-17 20:27:24 +0530

    FramerMotion

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import { motion, useInView } from "framer-motion";
+
+const fadeAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 70,
+  },
+  animate: {
+    opacity: 100,
+    y: 0,
+    transition: {
+      delay: 0.2,
+      duration: 0.8,
+    },
+  },
+};
 
 const Home = () => {
   return (
     <div class="Home">
       <Navbar />
-      <div className="container">
+      <motion.div
+        className="container"
+        variants={fadeAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+      >
         <div className="ls">
           <h1>
             Unlocking The Future<br></br> Through{" "}
@@ -23,7 +47,7 @@ const Home = () => {
         <div className="rs">
           <div className="img-p"></div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
