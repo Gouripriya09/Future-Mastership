bd86064569af7bc49ee1cd760e349ed5e46467be
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-17 20:03:17 +0530

    Hero Section 1

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,7 +6,24 @@ const Home = () => {
   return (
     <div class="Home">
       <Navbar />
-      <h1>HOME</h1>
+      <div className="container">
+        <div className="ls">
+          <h1>
+            Unlocking The Future<br></br> Through{" "}
+            <span className="col-class">Mastervance</span>.
+          </h1>
+          <p className="hero-sub">
+            Welcome to Mastervance, where we empower students to become masters
+            of tomorrow's world. Our platform is a gateway to the latest
+            advancements in technology.
+          </p>
+
+          <button className="ls-btn">Apply Now</button>
+        </div>
+        <div className="rs">
+          <div className="img-p"></div>
+        </div>
+      </div>
     </div>
   );
 };
