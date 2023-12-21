9d5bfd9fdcda172fbaae24ad99ca3870fd270efa
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-21 16:44:40 +0530

    Updates

---
+++ client/src/components/Course/CourseCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+const CourseCard = () => {
+  return <div className="course-card"></div>;
+};
+
+export default CourseCard;
--- client/src/pages/Courses/Courses.jsx
+++ client/src/pages/Courses/Courses.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import UD from "../../components/Development/UD";
-import "./Courses.css"
+import CourseCard from "../../components/Course/CourseCard";
+import "./Courses.css";
 
 const Courses = () => {
   return (
     <div className="light courses">
       <Navbar />
-      <UD name="Courses" />
+      <h1 className="col-class f-ss courses-heading">Free Courses</h1>
+      <div className="card-container">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
       <Footer />
     </div>
   );
