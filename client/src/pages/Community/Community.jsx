c4cacebfd0c854a6ad786f0e38a715d66f38306a
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-23 10:01:21 +0530

    Community page

--- client/src/pages/Community/Community.jsx
+++ client/src/pages/Community/Community.jsx
@@ -4,22 +4,31 @@ import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 import { motion } from "framer-motion";
 import underline from "../../assets/underline.png";
-import { Variant2 } from "../../utils/helpers";
+import { fadeAnimationVariants } from "../../utils/helpers";
 import community from "../../assets/community-ls.png";
 
 const Community = () => {
   return (
     <div className="Community light">
       <Navbar />
-      <motion.div className="container">
+      <motion.div
+        className="container"
+        variants={fadeAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{ once: true }}
+      >
         <div className="ls-c">
           <img src={community} />
         </div>
         <div className="rs-c">
           <h1>
             The choice<br></br> community for
-            <br /> <span className="col-class">digital workers</span>
-            <img src={underline} />
+            <br />{" "}
+            <div className="work-und">
+              <span className="col-class">digital workers</span>
+              <img src={underline} />
+            </div>
             <br /> and Mastervance
           </h1>
         </div>
