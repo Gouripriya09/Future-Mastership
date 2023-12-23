b17f0750d66ee90b6a4ff343ab79c51ced3a355b
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-23 09:38:21 +0530

    Progress to community page

--- client/src/pages/Community/Community.jsx
+++ client/src/pages/Community/Community.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import "./Community.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
-import UD from "../../components/Development/UD";
+import { motion } from "framer-motion";
+import underline from "../../assets/underline.png";
+import { Variant2 } from "../../utils/helpers";
+import community from "../../assets/community-ls.png";
 
 const Community = () => {
   return (
     <div className="Community light">
       <Navbar />
-      <UD name="Community" />
+      <motion.div className="container">
+        <div className="ls-c">
+          <img src={community} />
+        </div>
+        <div className="rs-c">
+          <h1>
+            The choice<br></br> community for
+            <br /> <span className="col-class">digital workers</span>
+            <img src={underline} />
+            <br /> and Mastervance
+          </h1>
+        </div>
+      </motion.div>
       <Footer />
     </div>
   );
