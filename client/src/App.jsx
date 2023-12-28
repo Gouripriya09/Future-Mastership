ffc0f71c09f6eba2cc63fed770c39809281c34ce
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-28 16:18:54 +0530

    Refund Policy, Dark Mode Functionality and minor responsivness

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Refund from "./pages/Refund_Policy/Refund";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import NoPage from "./pages/NoPage/NoPage";
@@ -10,8 +11,25 @@ import Auth from "./pages/Auth/Auth";
 import Courses from "./pages/Courses/Courses";
 
 const App = () => {
+  const initialMode = localStorage.getItem("themeMode") || "Light";
+  document.documentElement.classList.add(initialMode);
+
+  const [mode, setMode] = useState(initialMode);
+
+  const toggleDarkMode = () => {
+    const newMode = mode === "Dark" ? "Light" : "Dark";
+    localStorage.setItem("themeMode", newMode);
+    setMode(newMode);
+  };
+
+  useEffect(() => {
+    document.documentElement.classList.remove("Dark", "Light");
+    document.documentElement.classList.add(mode);
+  }, [mode]);
+
   return (
-    <div className="App" data-theme="light">
+    <div className="App">
+      <button onClick={toggleDarkMode}>Toggle Dark Mode</button>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,6 +37,7 @@ const App = () => {
           <Route path="/auth" element={<Auth />} />
           <Route path="/community" element={<Community />} />
           <Route path="/mentor" element={<Mentor />} />
+          <Route path="/refundpolicy" element={<Refund />} />
           <Route path="/courses" element={<Courses />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
