f4d1f5bffabdcba8632294764075f9672850ff7c
Yogeshwar-CM <cmyogeshwar@gmail.com>
2023-12-30 23:05:22 +0530

    Final Commit Completed Frontend

--- client/src/components/Course/CourseCard.jsx
+++ client/src/components/Course/CourseCard.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./style.css";
+import AI from "../../assets/AI.png";
 
 const CourseCard = () => {
-  return <div className="course-card"></div>;
+  return (
+    <div className="course-card">
+      <div className="top">
+        <img src={AI} alt="Course_Image" />
+      </div>
+      <div className="body">Artificial Intelligence</div>
+      <div className="bot">
+        <p className="bot-1">₹499</p>
+        <p className="bot-2"> Apply Now</p>
+      </div>
+    </div>
+  );
 };
 
 export default CourseCard;
