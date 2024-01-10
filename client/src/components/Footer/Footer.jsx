88f9866716d8b5ea4503e76c72e5300cd51996dd
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-01-10 18:22:23 +0530

    added the social links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -60,11 +60,41 @@ const Footer = () => {
         <div className="fc fc4">
           <p className="ul">Get In Touch</p>
           <div className="icons">
-            <i class="fa-brands fa-instagram"></i>
-            <i class="fa-brands fa-youtube"></i>
-            <i class="fa-brands fa-facebook"></i>
-            <i class="fa-brands fa-linkedin"></i>
-            <i class="fa-brands fa-square-x-twitter"></i>
+            <a
+              href="https://www.instagram.com/mastervance_16"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="unformat-link"
+            >
+              <i class="fa-brands fa-instagram"></i>
+            </a>
+            <a href="" className="unformat-link">
+              <i class="fa-brands fa-youtube"></i>
+            </a>
+            <a
+              href="https://www.facebook.com/profile.php?id=61555549581393&mibextid=ZbWKwL"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="unformat-link"
+            >
+              <i class="fa-brands fa-facebook"></i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/master-vance-4b97242a8/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="unformat-link"
+            >
+              <i class="fa-brands fa-linkedin"></i>
+            </a>
+            <a
+              href="https://twitter.com/MasterVanc44749"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="unformat-link"
+            >
+              <i class="fa-brands fa-square-x-twitter"></i>
+            </a>
           </div>
           <p className="ul">Contact Us</p>
           <p>
