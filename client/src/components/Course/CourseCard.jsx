f142316c56d7640ad3fb8baa108596807b4bebe1
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-01-15 07:45:47 +0530

    Updated Course Cards to new design

--- client/src/components/Course/CourseCard.jsx
+++ client/src/components/Course/CourseCard.jsx
@@ -1,20 +1,27 @@
 import React from "react";
 import "./style.css";
 import { Link } from "react-router-dom";
-import AI from "../../assets/AI.png";
+import AI from "../../assets/Course.png";
 
 const CourseCard = () => {
   return (
     <div className="course-card">
       <div className="top">
-        <img src={AI} alt="Course_Image" />
+        <img src={AI} alt="Course Image" />
       </div>
-      <div className="body">Artificial Intelligence</div>
+      <div className="body">
+        <Link to="/course" className="unformat-link">
+          <h2>Web Development | Complete FullStack Course | HTML CSS JS</h2>
+        </Link>
+        <div className="body-sub">
+          <p>Joshi Patel</p>
+          <p>
+            4.7 <i class="fa-solid fa-star"></i> (88 Reviews)
+          </p>
+        </div>
+      </div>
       <div className="bot">
         <p className="bot-1">₹499</p>
-        <Link to="/course" className="unformat-link">
-          <p className="bot-2"> Apply Now</p>
-        </Link>
       </div>
     </div>
   );
