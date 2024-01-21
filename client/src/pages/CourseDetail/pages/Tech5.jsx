4efd740b8df488fa066a8c3a9cbd128a44ae6090
Gouripriya09 <gourikpriya@gmail.com>
2024-01-21 17:51:21 +0530

    cmt

---
+++ client/src/pages/CourseDetail/pages/Tech5.jsx
@@ -0,0 +1,363 @@
+import React, { useState, useEffect, useContext } from "react";
+import Navbar from "../../../components/Navbar/Navbar";
+import Footer from "../../../components/Footer/Footer";
+import { motion } from "framer-motion";
+import { Link } from "react-router-dom";
+import { fadeAnimationVariants } from "../../../utils/helpers";
+import rev from "../../../assets/rev.png";
+import CourseCard from "../../../components/Course/CourseCard";
+import "./CourseDetail.css";
+
+const Tech5 = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+  return (
+    <div className="CourseDetail light">
+      <Navbar />
+      <motion.div
+        className="container"
+        id="hero-course"
+        variants={fadeAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+      >
+        <div className="left-course">
+          <h2>Digital Marketing Course</h2>
+          <div className="fot">
+            <div id="pfile"></div>
+            <p>Instructor Name</p>
+          </div>
+          <Link to="/payment" className="unformat-link">
+            <button className="enroll">Enroll</button>
+          </Link>
+        </div>
+        <div className="right-course">
+          <div className="course-box">
+            <div className="cb-1">
+              <p className="course-box-head">Digital Marketing Course</p>
+              <p className="course-box-desc">
+              Dive into the fundamental concepts with our Digital Marketing Course
+              </p>
+            </div>
+            <div className="cb-2">
+              <p>4.9 (135 Reviews) | 98%</p>
+              <ul className="course-box-list">
+                <li>
+                  <i
+                    className="fa-solid fa-circle-check"
+                    style={{ color: "#00c7fc" }}
+                  />
+                  <p>Beginner level</p>
+                </li>
+                <p className="sub">Gain Experience</p>
+                <li>
+                  <i
+                    className="fa-solid fa-circle-check"
+                    style={{ color: "#00c7fc" }}
+                  />
+                  <p>10 Hours Aprox</p>
+                </li>
+                <li>
+                  <i
+                    className="fa-solid fa-circle-check"
+                    style={{ color: "#00c7fc" }}
+                  />
+                  <p>Flexible Schedule</p>
+                </li>
+                <p className="sub">Learn at your own pace</p>
+                <li>
+                  <i
+                    className="fa-solid fa-circle-check"
+                    style={{ color: "#00c7fc" }}
+                  />
+                  <p>Earn degree credit</p>
+                </li>
+                <p className="sub">Learn more</p>
+              </ul>
+            </div>
+            <div className="cb-3">
+              <button className="cb-btn">Apply Now</button>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+        variants={fadeAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+      >
+        <div className="course-nav" id="course-nav">
+          <ul className="course-nav-ul">
+            <li className="course-nav-li">
+              <a href="#course-nav" className="unformat-link">
+                About
+              </a>
+            </li>
+            <li className="course-nav-li">
+              <a href="#what-c" className="unformat-link">
+                What you will learn
+              </a>
+            </li>
+            <li className="course-nav-li">
+              <a href="#content-c" className="unformat-link">
+                Content
+              </a>
+            </li>
+            <li className="course-nav-li">
+              <a href="#ass-c" className="unformat-link">
+                Assessment
+              </a>
+            </li>
+            <li className="course-nav-li">
+              <a href="#inst-c" className="unformat-link">
+                Instructor
+              </a>
+            </li>
+            <li className="course-nav-li">
+              <a href="#rev-c" className="unformat-link">
+                Reviews
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="about-c">
+          <h1>Overview</h1>
+          <p>
+          Explore the dynamic field of Digital Marketing with the comprehensive
+          <br />course. Tailored for marketers and business professionals. It covers <br />
+          key digital marketing strategies and tools. 
+          </p>
+        </div>
+        <div id="what-c">
+          <h1>What You Will Learn</h1>
+          <ul id="what-c-ul">
+            <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>
+              Understand the core principles of Digital Marketing.
+              </p>
+            </li>
+            {/* <p className="sub-c">
+              Explore the core concepts, history, and ethical considerations
+              surrounding Artificial Intelligence.
+            </p> */}
+            <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>
+              Apply various digital marketing strategies to promote products
+              <br />and services.
+              </p>
+            </li>
+            {/* <p className="sub-c">
+              Dive into the fundamentals of machine learning, covering
+              supervised and unsupervised learning, algorithms, and model
+              evaluation.
+            </p> */}
+            <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>
+               Gain practical experience in creating effective digital 
+               <br />marketing compaigns.
+              </p>
+            </li>
+            {/* <p className="sub-c">
+              Uncover the mysteries of neural networks, deep learning
+              architectures, and their applications in real-world scenarios.
+            </p> */}
+            {/* <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>Natural Language Processing (NLP):</p>
+            </li>
+            <p className="sub-c">
+              Learn how AI processes and understands human language, and explore
+              applications in text analysis, sentiment analysis, and language
+              generation.
+            </p>
+            <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>Computer Vision:</p>
+            </li>
+            <p className="sub-c">
+              Delve into the fascinating world of computer vision, understanding
+              how AI interprets and analyzes visual information.
+            </p>
+            <li id="what-c-li">
+              <i
+                className="fa-solid fa-circle-check rm"
+                style={{ color: "#00c7fc" }}
+              />
+              <p>Hands-On Projects:</p>
+            </li>
+            <p className="sub-c">
+              Apply your knowledge through hands-on projects that simulate
+              real-world AI applications, enhancing your problem-solving skills.
+          </p> */}
+          </ul>
+          <h1>Prerequisites</h1>
+          <p>A PC/Laptop with an Internet Connection.</p>
+        </div>
+        <div id="content-c">
+          <h1>Course Content:</h1>
+          <ul id="main-ul-c">
+            <li id="content-main-li">
+              Module 1: Introduction to Digital Marketing
+              <ul id="child-ul-c">
+                <li id="content-child-li">Overview of Digital Marketing channels</li>
+                <li id="content-child-li">Evalution of Digital Marketing</li>
+                <li id="content-child-li">
+                Digital Marketing Strategies and Objectives
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <ul id="main-ul-c">
+            <li id="content-main-li">
+              Module 2: Search Engine OptimiZation (SEO)
+              <ul id="child-ul-c">
+                <li id="content-child-li">
+                  Basics of SEO
+                </li>
+                <li id="content-child-li">On-page and Off-page SEO</li>
+                <li id="content-child-li">SEO Tools and Analytics</li>
+              </ul>
+            </li>
+          </ul>
+          <ul id="main-ul-c">
+            <li id="content-main-li">
+              Module 3: Social Media Marketing
+              <ul id="child-ul-c">
+                <li id="content-child-li">
+                  Leveraging Social Media Platforms for Marketing
+                </li>
+                <li id="content-child-li">Creating Engaging Content</li>
+                <li id="content-child-li">Social Media Analytics</li>
+              </ul>
+            </li>
+          </ul>
+          <ul id="main-ul-c">
+            <li id="content-main-li">
+              Module 4: Email Marketing
+              <ul id="child-ul-c">
+                <li id="content-child-li">Designing Effective Email Compaigns</li>
+                <li id="content-child-li">Email Automation and Segmentation</li>
+                <li id="content-child-li">Email Marketing Analytics</li>
+              </ul>
+            </li>
+          </ul>
+          <ul id="main-ul-c">
+            <li id="content-main-li">
+              Module 5: Pay-Per-Click (PPC) Advertising
+              <ul id="child-ul-c">
+                <li id="content-child-li">Introduction to PPC</li>
+                <li id="content-child-li">
+                  Craeting and Managing PPC Compaigns
+                </li>
+                <li id="content-child-li">
+                 PPC Analytics and Optimization
+                </li>
+                
+              </ul>
+            </li>
+          </ul>
+          
+        </div>
+        <div id="ass-c">
+          <h1>Assessment:</h1>
+          <p>Digital Marketing Comapign Projects: Develop and implement Digital 
+            <br />Marketing Compaigns.</p>
+          <p>SEO Analysis Assignments: Apply SEO techniques to optimize online content.
+          </p>
+          <p>Final Digital Marketing Project: Comprehensive assessment of Digital Marketing
+            <br />skills through a practical compaign.</p>
+        </div>
+        <div id="inst-c">
+          <h1>Instructor</h1>
+          <div className="inst">
+            <div className="pfile"></div>
+            <p className="inst-abt">
+              Juraj Šimunec is Microsoft 365 Engineer experienced with setup and
+              administration of complete M365 platform. His main area of focus
+              is to provide best possible experience for users, but at the same
+              time following best security practices in tenant configuration and
+              integration with various workloads.
+            </p>
+          </div>
+        </div>
+        <div id="rev-c">
+          <h1 id="rev-head">Reviews (40 Reviews)</h1>
+          <div className="ratings">
+            <p>Course Ratings</p>
+            <img src={rev} alt="Ratings" />
+          </div>
+        </div>
+      </motion.div>
+      <motion.div
+        className="container"
+        id="related-sec"
+        variants={fadeAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+      >
+        <h1>Other Courses</h1>
+        <div id="related">
+          <CourseCard
+            title="Web Development | Complete FullStack Course | HTML CSS JS"
+            name="Joshi Patel"
+            rating="4.7"
+            num="432"
+            price="599"
+          />
+          <CourseCard
+            title="Web Development | Complete FullStack Course | HTML CSS JS"
+            name="Joshi Patel"
+            rating="4.7"
+            num="432"
+            price="599"
+          />
+          <CourseCard
+            title="Web Development | Complete FullStack Course | HTML CSS JS"
+            name="Joshi Patel"
+            rating="4.7"
+            num="432"
+            price="599"
+          />
+          <CourseCard
+            title="Web Development | Complete FullStack Course | HTML CSS JS"
+            name="Joshi Patel"
+            rating="4.7"
+            num="432"
+            price="599"
+          />
+        </div>
+      </motion.div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Tech5;
