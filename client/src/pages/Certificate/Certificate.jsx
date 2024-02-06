a25e91601ed0edc6012f155884ea5eb77874b7f9
Yogeshwar-CM <cmyogeshwar@gmail.com>
2024-02-06 19:40:03 +0530

    made the verify certificate responsive

--- client/src/pages/Certificate/Certificate.jsx
+++ client/src/pages/Certificate/Certificate.jsx
@@ -4,29 +4,32 @@ import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+
 const Cert = () => {
-    return (
-        <div className="certificate">
-        <Navbar />
-<div className="cert">
-    <h3>Verify Certificate</h3>
-      <div className="cert1"> <p>To verify the legitimacy of any certificate issued <br />
-        by Mastervance please enter "Certificate Serial <br /> 
-        Number" given on your certificate.
-       </p>
-       </div>
-  <div className="input">
-    Certificate Serial Number
-    <div className="verify">
-    <input type="number" placeholder="Enter Number" />
-    <button className="button">verify</button>
+  return (
+    <div className="certificate light">
+      <Navbar />
+      <div className="cert">
+        <h3>Verify Certificate</h3>
+        <div className="cert1">
+          {" "}
+          <p>
+            To verify the legitimacy of any certificate issued by Mastervance
+            please enter "Certificate Serial Number" given on your certificate.
+          </p>
+        </div>
+        <div className="input">
+          Certificate Serial Number
+          <div className="verify">
+            <input type="number" placeholder="Enter Number" />
+            <button className="button">Verify</button>
+            <button className="button">Generate</button>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
-    
-  </div>
-</div>
-<Footer />
-</div>
-    );
+  );
 };
 
 export default Cert;
